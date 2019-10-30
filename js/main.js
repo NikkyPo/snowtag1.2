945ca9fc3df17e9f927cb4d2d81b06ebee22cceb
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-10-30 15:07:36 -0500

    leaflet points

--- js/main.js
+++ js/main.js
@@ -104,7 +104,7 @@ geotab.addin.snowtag = () => {
              map.fitBounds(bounds);
 
              var marker = L.Marker([coordinates])
-             console.log(marker);
+             console.log([marker]);
 
              toggleLoading(false);
              console.log("new coords", coordinates);
