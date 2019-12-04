38f92e0175e9cbb366a74028eb48afb2773e48d2
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-04 13:44:45 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -173,9 +173,7 @@ geotab.addin.snowtag = () => {
   //     displaySnowMap();
   // }
     document.getElementById('vehicles').addEventListener('change', event => {
-      $('#vehicles input:checked').each(function() {
-        console.log("check");
-      });
+      $("#check:checked").length;
       event.preventDefault();
       displaySnowMap();
     });
