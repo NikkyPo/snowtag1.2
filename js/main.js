398da053dc9b69976615e915d9fdb9116fb2a1e4
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-04 11:26:24 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -232,7 +232,7 @@ geotab.addin.snowtag = () => {
           option.value = vehicle.id;
           option.id = 'check'
 
-          option.addEventListener("click", dynamicEvent, false);
+          option.addEventListener("change", dynamicEvent, false);
 
           elVehicleSelect.appendChild(option);
           elVehicleSelect.insertAdjacentText('beforeend', option.name);
