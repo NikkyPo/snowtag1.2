453ec3370c9f3237392e5f193aeb43edb8fd2c63
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-04 16:08:52 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -176,14 +176,15 @@ geotab.addin.snowtag = () => {
       var checkedCbs = document.querySelectorAll('#vehicles input[type="checkbox"]:checked');
       for (var i=0; i < checkedCbs.length; i++) {
               checkedCbs[i].onchange = function() {
-                  if (this.checked) {
-                    console.log("passes")
-                    ids = checkedCbs[i].value;
-                    event.preventDefault();
-                    displaySnowMap();
-                  } else {
-                      snowMapLayer.clearLayers();
-                  }
+                console.log("hi")
+                  // if (this.checked) {
+                  //   console.log("passes")
+                  //   ids = checkedCbs[i].value;
+                  //   event.preventDefault();
+                  //   displaySnowMap();
+                  // } else {
+                  //     snowMapLayer.clearLayers();
+                  // }
               }
       }
 
