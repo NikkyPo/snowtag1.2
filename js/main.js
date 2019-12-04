c5422e29362fad229bbf46090168822db608feb4
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-04 17:36:33 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -183,7 +183,7 @@ geotab.addin.snowtag = () => {
                     displaySnowMap();
                   } else {
                       console.log("off")
-                      snowMapLayer.removeLayer(this.value);
+                      snowMapLayer.removeLayer(ids);
                   }
               }
       }
