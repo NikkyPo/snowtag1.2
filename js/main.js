9af0cc380cf492b443a8f70ef78789ad3b09b082
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-06 11:49:22 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -111,7 +111,8 @@ geotab.addin.snowtag = () => {
            console.log("new points",coordinates)
            for (var i = 0; i < coordinates.length; i++) {
              map.fitBounds(bounds);
-             var newCoords[coordinates[i].id] = L.circleMarker([coordinates[i].lat,coordinates[i].lon], {
+             var newCoords = {}
+             newCoords[coordinates.id] = L.circleMarker([coordinates[i].lat,coordinates[i].lon], {
                color: "red",
                fillColor: "#f03",
                fillOpacity: 0.5,
