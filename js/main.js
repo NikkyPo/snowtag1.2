de8ab5376573d43dba41efd62b1d455880929dda
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-09 17:05:22 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -202,7 +202,7 @@ geotab.addin.snowtag = () => {
           console.log("on 2", input)
           ids = this.value;
           snowMapLayer.eachLayer((layer) => {
-            console.log("working2")
+            console.log(layer, ids)
             if (layer.options.uniqueID === ids) {
               console.log("layers off2")
               snowMapLayer.removeLayer(layer)
