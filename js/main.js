7395e53c847a6a99cc780baf467f81627970a118
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-10 15:52:18 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -178,8 +178,17 @@ geotab.addin.snowtag = () => {
     elDateToInput.value = yy + '-' + mm + '-' + dd + 'T' + '23:59';
 
     // events
-    checkedCbs = document.querySelectorAll('#vehicles input[type="checkbox"]');
-    checkedCbs.addEventListener('change', event => {
+
+    document.getElementById('vehicles').addEventListener('change', e => {
+
+      if(e.target && e.target.nodeName == '#vehicles input[type="checkbox"]') {
+    		// List item found!  Output the ID!
+    		console.log("clicked!");
+    	}
+
+
+
+      checkedCbs = document.querySelectorAll('#vehicles input[type="checkbox"]');
       for (var i=0; i < checkedCbs.length; i++) {
               checkedCbs[i].onchange = function() {
                   if (this.checked) {
