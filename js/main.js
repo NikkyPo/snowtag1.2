bbd82643351f47d2328a59dfa3bcd880c655fc79
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-10 14:14:28 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -184,17 +184,14 @@ geotab.addin.snowtag = () => {
         if (checkedCbs.checked) {
           console.log("on1")
           ids = checkedCbs.value;
-
-
           snowMapLayer.eachLayer((layer) => {
-            if (snowMapLayer.hasLayer(layer.options.uniqueID === ids)) {
+            if (layer.options.uniqueID === ids) {
+              return;
+            } else {
               event.preventDefault();
-              displaySnowMap();
+              displaySnowMap();            
             }
           }
-
-
-
         } else {
           document.getElementById("error").innerHTML = "";
             console.log("off1")
