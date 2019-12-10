2654c3b945c2faac1719c99b6a52efa0546e2cff
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-10 16:12:35 -0600

    html change

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ geotab.addin.snowtag = () => {
 
   let getColor = d => {
     console.log("color", d)
-    return d.match(/B/) !== false ? '#FED976' :
+    return d.match(/B/) !== true ? '#FED976' :
                                     '#FFEDA0';
   }
 
