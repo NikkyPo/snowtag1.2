7c8e52e48efdbfb0976a7570748f9417161b5d3e
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-11 17:11:27 -0600

    leaflet icons

--- js/main.js
+++ js/main.js
@@ -312,7 +312,7 @@ geotab.addin.snowtag = () => {
                     ids = this.value;
                     event.preventDefault();
                     displaySnowMap();
-                    console.log(this.checked)
+                    console.log(checkedCbs)
 
 
 
