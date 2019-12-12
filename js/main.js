66a3e00045752ad6e1f0b4ba7aa837f7767f91d5
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-12-12 15:25:56 -0600

    leaflet icons

--- js/main.js
+++ js/main.js
@@ -443,7 +443,7 @@ geotab.addin.snowtag = () => {
           option.setAttribute('type', 'checkbox')
           option.name = vehicle.name;
           option.value = vehicle.id;
-          option.className = 'checkmateFormEditField checkbox'
+          option.className = 'checkmateFormEditField'
 
           elVehicleSelect.appendChild(option);
           elVehicleSelect.insertAdjacentText('beforeend', option.name);
