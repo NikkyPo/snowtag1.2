3925e3add8eed540196dd585dbab675ed414b000
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2020-01-02 09:23:43 -0600

    change in html file name

--- js/main.js
+++ js/main.js
@@ -42,9 +42,9 @@ geotab.addin.snowtag = () => {
 
 // returns color based on id properties
   let getColor = d => {
-    return d == d.match(/[a]/gi) ? redIcon :
-           d == d.match(/[b]/gi) ? greenIcon :
-           d == d.match(/[c]/gi) ? blueIcon :
+    return d == d.match(/[b2B]/gi) ? redIcon :
+           d == d.match(/[b2A]/gi) ? greenIcon :
+           d == d.match(/[b29]/gi) ? blueIcon :
            d == d.match(/[3-4]/gi) ? orangeIcon :
            d == d.match(/[5-6]/gi) ? yellowIcon :
            d == d.match(/[7-8]/gi) ? violetIcon :
